59edc092298e14fb9663eb08ae02f68ebba0aa50
dx862176 <Dave.Dockhorn@digitalglobe.com>
2015-12-17 13:43:48 -0700

    adding python test file

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
-import sys, os
+import sys
+import os
 
 def main():
-	try:
-		print 'This is a fake test.'
-		sys.exit(0)
-	except:
-		sys.exit(1)
+    try:
+        print 'This is a fake test.'
+        sys.exit(0)
+    except:
+        sys.exit(1)
 
 if __name__ == '__main__':
-	main()
+    main()
