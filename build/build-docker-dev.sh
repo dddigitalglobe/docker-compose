306c7a776984189e157bd9db51912da3c3e09981
dx862176 <Dave.Dockhorn@digitalglobe.com>
2015-12-17 10:06:31 -0700

    removing spaces around =

--- build/build-docker-dev.sh
+++ build/build-docker-dev.sh
@@ -1,5 +1,7 @@
-dockGuid = "$(docker images | grep dockercompose_odoo | awk '{print $3}')"
-docker tag $dockGuid dddigitalglobe/dockercompose_odoo:latest
+dockGuid=`docker images | grep dockercompose_odoo | awk '{print $3}'`
+echo "${dockGuid}"
+docker tag "${dockGuid}" dddigitalglobe/dockercompose_odoo:latest
 
-dockGuid2 = "$(docker images | grep dockercompose_db | awk '{print $3}')"
-docker tag $dockGuid2 dddigitalglobe/dockercompose_db:latest
+dockGuid2=`docker images | grep dockercompose_db | awk '{print $3}'`
+echo "$dockGuid2"
+docker tag "${dockGuid2}" dddigitalglobe/dockercompose_db:latest
