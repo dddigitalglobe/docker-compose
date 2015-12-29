edd605bda70fc451ffd34d72d1ff63795ebd1117
Dave Dockhorn <dave.dockhorn@digitalglobe.com>
2015-12-29 13:58:45 -0700

    Update nightwatch.js

--- tests/nightwatch.js
+++ tests/nightwatch.js
@@ -1,18 +1,3 @@
-{
-  "src_folders" : ["./tests"],
-  "output_folder" : "./reports",
-  "custom_commands_path" : "",
-  "custom_assertions_path" : "",
-  "globals_path" : "",
-
-  "selenium" : {
-    "start_process" : true,
-    "server_path" : "",
-    "log_path" : "",
-    "host" : "127.0.0.1",
-    "port" : 4444
-  },
-
   "test_settings" : {
     "default" : {
       "launch_url" : "http://localhost",
